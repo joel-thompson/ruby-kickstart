83da1cdbb778b4c1c9692d856cebc20ec4a5ebeb
chuckwalla <jeelthompson@gmail.com>
2016-08-29 06:37:18 -0700

    starting 3-5

--- session3/3-challenge/5_blocks.rb
+++ session3/3-challenge/5_blocks.rb
@@ -19,11 +19,27 @@
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
-def spiral_access(array)
+def spiral_access(array, &block)
+
+	p array
+
+	#first figure out how to print each element in a spiral
 
 	
 	
 end
 
 
+
+two_d = [
+  [ 1,  2,  3,  4, 5],
+  [16, 17, 18, 19, 6],
+  [15, 24, 25, 20, 7],
+  [14, 23, 22, 21, 8],
+  [13, 12, 11, 10, 9],
+]
+order = []
+spiral_access two_d do |i|
+  order << i
+end
 
